a1e1328d3806fde3f60f364e760d9c2bf1d961e8
cuibinge <44290497+cuibinge@users.noreply.github.com>
2025-03-01 17:01:41 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,3 @@ DEFAULT_IMAGE_TOKEN = "<image>"
 DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
 DEFAULT_IM_START_TOKEN = "<im_start>"
 DEFAULT_IM_END_TOKEN = "<im_end>"
-
-from .build import build_model, build_vlm_model
-from .text_modal import tokenizer_image_token
