f3b37c79a46abb2b6da06b8d70c961fa02e30fe5
Yangkdi <1634032454@qq.com>
2025-03-30 10:12:02 +0800

    Delete serve directory

--- serve/gradio_trial.py
+++
@@ -1,6 +0,0 @@
-#这段代码使用了 Python 的 gradio 库来创建一个简单的 Web 应用程序，允许用户输入文本并获取一个问候语作为响应.用于演示如何使用 Gradio 库快速创建一个交互式的 Web 应用程序。
-import gradio as gr
-def greet(name):
-    return "Hello " + name + "!"
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch(share=True)
