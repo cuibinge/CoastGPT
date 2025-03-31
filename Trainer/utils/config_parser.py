263c6595132a1fb99d1393fe8a641f73744d70a6
YangKdi <1634032454@qq.com>
2025-03-31 21:12:08 +0800

    Update config_parser.py

--- Trainer/utils/config_parser.py
+++ Trainer/utils/config_parser.py
@@ -19,7 +19,7 @@ class ConfigArgumentParser(argparse.ArgumentParser):
         self.config_parser.add_argument(
             "-c",
             "--config",
-            default="Configs/inference.yaml",
+            default="Configs/train.yaml",
             metavar="FILE",
             help="where to load YAML configuration",
         )
