0c204ae4ec676435d1845cc1dfde2dfcee55410c
KaiCcc <1660695450@qq.com>
2025-05-28 18:36:17 +0800

    Update utils.py

--- Models/utils.py
+++ Models/utils.py
@@ -45,7 +45,8 @@ class KeywordsStoppingCriteria(StoppingCriteria):
             keyword_id.to(output_ids.device) for keyword_id in self.keyword_ids
         ]
         for keyword_id in self.keyword_ids:
-            if output_ids[0, -keyword_id.shape[0] :] == keyword_id:
+            # if output_ids[0, -keyword_id.shape[0] :] == keyword_id:
+            if torch.all(output_ids[0, -keyword_id.shape[0]:] == keyword_id):
                 return True
         outputs = self.tokenizer.batch_decode(
             output_ids[:, -offset:], skip_special_tokens=True
